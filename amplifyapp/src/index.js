1fa6c9c6cde643409b580a34519bd434b6bd8a43
Andre Nunes <andre.nunes@vwds.pt>
2023-01-20 14:48:21 +0000

    App auth

--- amplifyapp/src/index.js
+++ amplifyapp/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Amplify } from "aws-amplify";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import config from "./aws-exports";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+Amplify.configure(config);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
